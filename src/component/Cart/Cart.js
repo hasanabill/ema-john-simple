abe2944f172eaafd5f4bf1ce274865d28fa20cc0
hasanabil <mahmudul.anabil2862@gmail.com>
2022-03-25 20:17:39 +0600

    finished

--- src/component/Cart/Cart.js
+++ src/component/Cart/Cart.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import './Cart.css'
 
-const Cart = (props) => {
+const Cart = ({ cart }) => {
+
+    let total = 0;
+    let shipping = 0;
+    let quantity = 0;
+    for (const product of cart) {
+        quantity = quantity + product.quantity;
+        total = total + product.price * product.quantity;
+        shipping = shipping + product.shipping;
+    }
+
+    const tax = (total * 0.1).toFixed(2);
+    const grandTotal = total + shipping + parseFloat(tax);
+
     return (
-        <div>
+        <div className='cart'>
             <h4>Order Summery</h4>
-            <p>Selected Items: {props.cart.length}</p>
+            <p>Selected Items: {quantity}</p>
+            <p>Total Price: ${total}</p>
+            <p>Total Shipping: ${shipping}</p>
+            <p>Tax: ${tax}</p>
+            <h5>Grand Total: ${grandTotal}</h5>
 
         </div>
     );
